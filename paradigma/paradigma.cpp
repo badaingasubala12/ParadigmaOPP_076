6dc7e5832674874132f40b0ee78f92d4e470e003
badai ngasubala <badaingasubala12@gmail.com>
2023-05-24 21:15:48 +0700

    menjalankan

--- paradigma/paradigma.cpp
+++ paradigma/paradigma.cpp
@@ -8,10 +8,4 @@ using namespace std;
 
  
 
-int main() {
-	pelajar siswa1("andi laksono", "SMAN 1 Bantul");
-	cout << siswa1.perkenalan();
-	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
-
-	return 0;
-}
+ 
